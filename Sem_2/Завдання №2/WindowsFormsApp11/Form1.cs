a417f428126a2fa8bdb3777ec9e97d85997a5e32
Dima <dima24sokyrko@gmail.com>
2020-06-01 12:51:12 +0300

    Sem_2

---
+++ Sem_2/Завдання №2/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += Button_Move;
+            this.button1.MouseEnter += Button_OK_MouseEnter;
+            this.button1.TabStop = false;
+            Text = " ";
+        }
+
+        private void Button_Move(object sender, MouseEventArgs e)
+        {
+            int step_distance = 2;
+            int field_distance_from_the_button = 30;
+            int button_width = 100;
+            int button_height = 50;
+            int cursor_position_x = e.X;
+            int cursor_position_y = e.Y;
+            int button_position_x = button1.Location.X;
+            int button_position_y = button1.Location.Y;
+            bool move = false;
+
+            if (cursor_position_x > button_position_x - field_distance_from_the_button && cursor_position_y > button_position_y - field_distance_from_the_button && cursor_position_x < button_position_x && cursor_position_y < button_position_y)
+            {
+                button1.Location = new Point(button_position_x + step_distance, button_position_y + step_distance);
+                move = true;
+            }
+            else if (cursor_position_x < button_position_x + button_width && cursor_position_y > button_position_y - field_distance_from_the_button && cursor_position_x > button_position_x && cursor_position_y < button_position_y)
+            {
+                button1.Location = new Point(button_position_x, button_position_y + step_distance);
+                move = true;
+            }
+            else if (cursor_position_x < button_position_x + button_width + field_distance_from_the_button && cursor_position_y > button_position_y - field_distance_from_the_button && cursor_position_x > button_position_x + button_width && cursor_position_y < button_position_y)
+            {
+                button1.Location = new Point(button_position_x - step_distance, button_position_y + step_distance);
+                move = true;
+            }
+            else if (cursor_position_x > button_position_x - field_distance_from_the_button && cursor_position_y < button_position_y + button_height && cursor_position_x < button_position_x && cursor_position_y > button_position_y)
+            {
+                button1.Location = new Point(button_position_x + step_distance, button_position_y);
+                move = true;
+            }
+            else if (cursor_position_x < button_position_x + button_width + field_distance_from_the_button && cursor_position_y < button_position_y + button_height && cursor_position_x > button_position_x + button_width && cursor_position_y > button_position_y)
+            {
+                button1.Location = new Point(button_position_x - step_distance, button_position_y);
+                move = true;
+            }
+            else if (cursor_position_x > button_position_x - field_distance_from_the_button && cursor_position_y < button_position_y + button_height + field_distance_from_the_button && cursor_position_x < button_position_x && cursor_position_y > button_position_y + button_height)
+            {
+                button1.Location = new Point(button_position_x + step_distance, button_position_y - step_distance);
+                move = true;
+            }
+            else if (cursor_position_x < button_position_x + button_width && cursor_position_y < button_position_y + button_height + field_distance_from_the_button && cursor_position_x > button_position_x && cursor_position_y > button_position_y + button_height)
+            {
+                button1.Location = new Point(button_position_x, button_position_y - step_distance);
+                move = true;
+            }
+            else if (cursor_position_x < button_position_x + button_width + field_distance_from_the_button && cursor_position_y < button_position_y + button_height + field_distance_from_the_button && cursor_position_x > button_position_x + button_width && cursor_position_y > button_position_y + button_height)
+            {
+                button1.Location = new Point(button_position_x - step_distance, button_position_y - step_distance);
+                move = true;
+            }
+
+
+
+
+
+            field_distance_from_the_button = 150;
+            if (button_position_x + button_width > this.Size.Width)
+            {
+                button_position_x -= field_distance_from_the_button;
+                button1.Location = new Point(button_position_x, button_position_y);
+            }
+            else if (button_position_y + button_height > this.Size.Height)
+            {
+                button_position_y -= field_distance_from_the_button;
+                button1.Location = new Point(button_position_x, button_position_y);
+            }
+            else if (button_position_x < 0)
+            {
+                button_position_x += field_distance_from_the_button;
+                button1.Location = new Point(button_position_x, button_position_y);
+            }
+            else if (button_position_y < 0)
+            {
+                button_position_y += field_distance_from_the_button;
+                button1.Location = new Point(button_position_x, button_position_y);
+            }
+
+        }
+
+        private void Button_OK_MouseEnter(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int x = this.button1.Location.X;
+            int y = this.button1.Location.Y;
+            int delta = rand.Next(50, 100);
+            x = (x < this.Size.Width / 2 ? x -= delta : x += delta);
+            y = (y < this.Size.Height / 2 ? y -= delta : y += delta);
+
+            button1.Location = new Point(x, y);
+        }
+
+
+        private void Button_Cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
---
+++ Sem_2/Завдання №3/WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(576, 309);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Завдання 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(576, 309);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(28, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Silver;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 68);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(240, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Додати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(369, 21);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Видалити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.ShowToolTips = true;
+            this.tabControl1.Size = new System.Drawing.Size(584, 338);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
